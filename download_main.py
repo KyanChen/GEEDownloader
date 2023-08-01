b41bfc5a039bb8316637e8af568d8a18c7f960b9
陈科研 <chenkeyan@xiaohongshu.com>
2023-08-01 22:49:08 +0800

    add

--- download_main.py
+++ download_main.py
@@ -36,46 +36,51 @@ def to_4326_osr(x, y, epsg=None, zone_number=None, is_northern_hemisphere=True):
     return point.GetX(), point.GetY()
 
 def download_tif_by_productid(data, vis=False):
-    collections = ee.ImageCollection('COPERNICUS/S2_SR')
-    product_id = data['S2_PRODUCT_ID']
-    geometry = data['geometry']
-    img_collection = collections.filter(ee.Filter.eq('PRODUCT_ID', product_id))
-    assert img_collection.size().getInfo() == 1
-    img = img_collection.first()
-    img = img.select('B2', 'B3', 'B4')
+    try:
+        collections = ee.ImageCollection('COPERNICUS/S2_SR')
+        product_id = data['S2_PRODUCT_ID']
+        geometry = data['geometry']
+        img_collection = collections.filter(ee.Filter.eq('PRODUCT_ID', product_id))
+        assert img_collection.size().getInfo() == 1
+        img = img_collection.first()
+        # img = img.select('B2', 'B3', 'B4')
 
-    geometry_utm = geometry.split('((')[1].split('))')[0]
-    geometry_utm = geometry_utm.replace(', ', ',').replace(' ', ',')
-    geometry_utm = geometry_utm.split(',')
-    geometry_utm = [[float(geometry_utm[i]), float(geometry_utm[i + 1])] for i in range(0, len(geometry_utm), 2)]
-    geometry = [to_proj_4326(data['crs'], *x) for x in geometry_utm]
-    geometry = [[x[1], x[0]] for x in geometry]
-    mean_lon = np.mean([x[0] for x in geometry[0:4]])
-    mean_lat = np.mean([x[1] for x in geometry[0:4]])
-    assert np.abs(mean_lon - data['lon']) < 0.001
-    assert np.abs(mean_lat - data['lat']) < 0.001
+        geometry_utm = geometry.split('((')[1].split('))')[0]
+        geometry_utm = geometry_utm.replace(', ', ',').replace(' ', ',')
+        geometry_utm = geometry_utm.split(',')
+        geometry_utm = [[float(geometry_utm[i]), float(geometry_utm[i + 1])] for i in range(0, len(geometry_utm), 2)]
+        geometry = [to_proj_4326(data['crs'], *x) for x in geometry_utm]
+        geometry = [[x[1], x[0]] for x in geometry]
+        mean_lon = np.mean([x[0] for x in geometry[0:4]])
+        mean_lat = np.mean([x[1] for x in geometry[0:4]])
+        assert np.abs(mean_lon - data['lon']) < 0.001
+        assert np.abs(mean_lat - data['lat']) < 0.001
 
-    zone_number = int(data['utm'].split('N')[0])
-    is_northern_hemisphere = True if data['utm'].split('N')[1] == 'N' else False
-    geometry_osr = [to_4326_osr(*x, None, zone_number, is_northern_hemisphere) for x in geometry_utm]
-    assert np.sum(np.abs(np.array(geometry) - np.array(geometry_osr)) > 0.00001)
+        zone_number = int(data['utm'].split('N')[0])
+        is_northern_hemisphere = True if data['utm'].split('N')[1] == 'N' else False
+        geometry_osr = [to_4326_osr(*x, None, zone_number, is_northern_hemisphere) for x in geometry_utm]
+        assert np.sum(np.abs(np.array(geometry) - np.array(geometry_osr)) > 0.00001)
 
-    geometry = ee.Geometry.Polygon(geometry)
+        geometry = ee.Geometry.Polygon(geometry)
 
-    img_filename = data['save_folder'] + '/' + data['dw_id']
-    geemap.download_ee_image(img, filename=img_filename + '.tif', scale=10, region=geometry)
-    if vis:
-        # vis rgb bands
-        rgb_img = visualize_tif(img_filename + '.tif')
-        cv2.imwrite(img_filename + '.png', rgb_img)
-        # vis label
-        label_filename = data['labeler'].replace('-', '_') + '/' + data['hemisphere'] + '/' + str(data['biome']) + '/' + data['dw_id'] + '.tif'
-        rgb_label = visualize_label(label_filename)
-        plt.imsave(img_filename + '_label.png', rgb_label)
+        img_filename = data['save_folder'] + '/' + data['dw_id']
+        geemap.download_ee_image(img, filename=img_filename + '.tif', scale=10, region=geometry)
+        if vis:
+            # vis rgb bands
+            rgb_img = visualize_tif(img_filename + '.tif', bounds=[1, 2, 3])
+            cv2.imwrite(img_filename + '.png', rgb_img)
+            # vis label
+            label_filename = 'original_files' + '/' + data['labeler'].replace('-', '_') + '/' + data['hemisphere'] + '/' + str(data['biome']) + '/' + data['dw_id'] + '.tif'
+            rgb_label = visualize_label(label_filename)
+            plt.imsave(img_filename + '_label.png', rgb_label)
+    except Exception as e:
+        print(e)
+        print(data['dw_id'])
 
-def visualize_tif(tif_file):
+
+def visualize_tif(tif_file, bounds):
     dataset = gdal.Open(tif_file)
-    img_gdal = dataset.ReadAsArray()
+    img_gdal = dataset.ReadAsArray()[bounds]
     img_gdal = img_gdal.transpose(1, 2, 0)
     img_gdal = img_gdal / 8
     img_gdal[img_gdal > 255] = 255
@@ -97,14 +102,14 @@ def visualize_label(label_filename):
 
 
 if __name__ == '__main__':
-    xlsx_file = '副本v1_dw_tile_metadata_for_public_release1.xlsx'
+    xlsx_file = 'original_files/v1_dw_tile_metadata_for_public_release.xlsx'
     save_folder = 'downloaded_tif'
-    num_workers = 32
+    num_workers = 256
     os.makedirs(save_folder, exist_ok=True)
 
     data = pd.read_excel(xlsx_file)
     data_list = []
-    for i in range(100):
+    for i in range(len(data)):
         tmp_data_dict = {}
         row = data.iloc[i].to_dict()
         tmp_data_dict.update(row)
